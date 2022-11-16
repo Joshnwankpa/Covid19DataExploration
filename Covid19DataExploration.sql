0669678cd9cfc474362bc5a8c7aa8c20ff2de4db
Joshua Nwankpa <115891296+Joshnwankpa@users.noreply.github.com>
2022-11-16 23:41:24 +0000

    Update Covid19DataExploration.sql

--- Covid19DataExploration.sql
+++ Covid19DataExploration.sql
@@ -170,36 +170,3 @@ Join PortfolioProject..CovidVaccination1 vac
 	On dea.location = vac.location
 	and dea.date = vac.date
 where dea.continent is not null
-
-
--- Visualisation Code
-
-Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
-From PortfolioProject..CovidDeaths1
---Where location = 'Brazil'
-where continent is not null 
---Group By date
-order by 1,2
-
-
-Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
-From PortfolioProject..CovidDeaths1
---Where location = 'Brazil'
-Where continent is null 
-and location not in ('World', 'European Union', 'International')
-Group by location
-order by TotalDeathCount desc
-
-
-Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths1
---Where location = 'Brazil'
-Group by Location, Population
-order by PercentPopulationInfected desc
-
-
-Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
-From PortfolioProject..CovidDeaths1
---Where location = 'Brazil'
-Group by Location, Population, date
-order by PercentPopulationInfected desc
